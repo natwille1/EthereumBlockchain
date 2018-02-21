33bfc5d24fb14302fa8e4e4ef0401b4705f27657
jbarkhan <33685871+jbarkhan@users.noreply.github.com>
2018-02-21 12:10:07 +1100

    added dividend functionality

--- MyTokenTemp.sol
+++ MyTokenTemp.sol
@@ -1,13 +1,18 @@
 contract MyTokenTemp {
     /* This creates an array with all balances */
     mapping (address => uint256) public balanceOf;
+    
+    address[] public divAddress = new address[](10);           // store addresses of token purchasers
+    
     uint256 public tokenValue;
-    uint256 private beneficiary;
+    uint256 public beneficiary;
+    unit256 public totalSupply;
 
     function MyTokenTemp(uint256 initialSupply, uint256 value) {
         balanceOf[msg.sender] = initialSupply;              // Give the creator all initial tokens
         tokenValue = value;                                 // set value of token
-        beneficiary = msg.sender                            // store address of contract instantiator 
+        beneficiary = msg.sender;                       // store address of contract instantiator 
+        totalSupply = intialSupply;
     }
 
     /* Send coins */
@@ -18,4 +23,41 @@ contract MyTokenTemp {
         beneficiary.transfer(_value * tokenValue * 10 ** 18);         // Send ether from buyer to owner
         balanceOf[benficiary] -= _value;                             // Subtract from the sender
         balanceOf[msg.sender] += _value;                                // Add the same to the recipient
+        
+        if(checkTokenHolder(msg.sender) == false){                       // add buyer address to list of token holders
+            addTokenHolder(msg.sender);    
+        }
+
+    /* supply dividends */
+    function supplyDiv() onlyOwner {
+        for(uint i = 0; i < 10; i++){
+            recipient = divAddress[i];
+            if(recipient == 0x0){
+                return;
+            }
+            else{
+                recipient.transfer(beneficiary.balance * balanceOf[recipient] / totalSupply)
+            }
+        }
+    }
+
+    /* add address to list of token holders */
+    function addTokenHolder(address buyer) private {
+        for(uint i = 0; i < 10; i++){
+            if(divAddress[i] == 0x0){
+                divAddress[i] = buyer;
+                return;
+            }
+        }
+    }
+
+    /* check if address is a token holder */
+    function checkTokenHolder(address buyer) private returns(bool) {
+        for(uint i = 0; i < 10; i++){
+            if(divAddress[i] == buyer){
+                return true;
+            }
+        }
+        return false;
+    }
 }
